26397566d365499b67e481ad6a6bfdce348db57e
Fenar Khan <fenarkhan@gmail.com>
2024-02-08 15:47:12 +0530

    4

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero"
 import Camp from "@/components/Camp"
-import Guide from "@/components/Hero"
+import Guide from "@/components/Guide"
 import Features from "@/components/Features"
 import GetApp from "@/components/GetApp"
 

291a4c675fb2e9c1a013990ec996f3d7f0914af9
Fenar Khan <fenarkhan@gmail.com>
2024-02-10 12:56:39 +0530

    7

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from 'react'
+import Image from 'next/image'
 
 interface CampProps{
   backgroundImage: string;
@@ -10,7 +12,20 @@ interface CampProps{
 const CampSite = ({backgroundImage, title, subtitle, peopleJoined}:CampProps) =>{
   return(
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage}
-    bg-cover bg-no-repeat lg:rounded-r-5x1 2x1:rounded-5x1`}>Camsite</div>
+    bg-cover bg-no-repeat lg:rounded-r-5x1 2x1:rounded-5x1`}>
+      <div className='flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10'>
+        <div className='flexCenter gap-4'>
+          <div className='rounded-full bg-green-50 p-4'>
+            <Image
+            src="/folded-map.svg"
+            alt = "map"
+            width={28}
+            height={28}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
